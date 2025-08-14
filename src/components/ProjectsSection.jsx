2e4caf97b5ba31282bfa0a225fb55878920189a2
m7md3baset74 <mohamedabdelbasset265@gmail.com>
2025-08-14 16:59:30 +0300

    Done

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -41,7 +41,7 @@ export default function ProjectsSection() {
     <div className='container mx-auto max-w-5xl'>
         <h2 className='text-3xl md:text-4xl font-bold mb-4 text-center'>Featured 
             <span className='text-primary'> Projects</span></h2>
-        <p className='text-center text-primary-foreground mb-12 max-w-2xl mx-auto'>
+        <p className='text-center text-muted-foreground mb-12 max-w-2xl mx-auto'>
             Here are some of my recent projects. Each project was carefully
             crafted with attention to detail, performance, and user experience.
         </p>
